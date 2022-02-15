7c7269c89c7da12bdd37376461cf4169f9b53f26
Timothee Guerin <tiguerin@microsoft.com>
2022-02-14 16:02:51 -0800

    First commit

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import yargs from "yargs";
+async function main() {
+    await yargs(process.argv.slice(2));
+}
+main().catch((error) => {
+    console.error("Uncaught error occurred");
+    console.error();
+    console.error(error);
+    process.exit(1);
+});
+//# sourceMappingURL=cli.js.map
